cbd0b4dc4bb29ab3094afaaea86a2fa1a7ef65c4
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 15:24:16 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+include "../../premake/common_premake_defines.lua"
+
+project "simdjson"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./**.h",
+		"./src/simdjson.cpp"
+	}
+
+	includedirs {
+		"%{IncludeDir.simdjson}"
+	}
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"

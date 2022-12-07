732693e07f4c5bd9e899ac3e992dce83e7eb6eb3
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:26 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -16,22 +16,3 @@ project "simdjson"
 	includedirs {
 		"%{IncludeDir.simdjson}"
 	}
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
